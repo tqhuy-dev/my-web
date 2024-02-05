1d719c927c2c09b6c7d445e4b8ac2d3562fb254e
huytrantech <133479098+huytrantech@users.noreply.github.com>
2024-02-05 15:01:56 +0700

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="common-container"></div>
+      <Nav className='menu-parent'>
+        <Nav.Item>
+          <Nav.Link className='menu-item' href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='menu-item' eventKey="link-1">Services</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='menu-item' eventKey="link-2">Topics</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='menu-item' eventKey="link-2">Experience</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className='menu-item' eventKey="link-2">Portfolio</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
